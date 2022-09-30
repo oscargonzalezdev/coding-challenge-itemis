76518184b30e49e94c0f83c4761afcf1196eafc3
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-09-30 21:37:53 +0200

    feat(App): add navbar and solutions component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import ChallengeSolutions from "./components/ChallengeSolutions";
 import ReceiptCalculator from "./components/ReceiptCalculator";
+import { Link, Divider, Text } from '@chakra-ui/react'
 
 function App() {
   return (
-    <>
+    <div className="container">
+      <nav className="navbar">
+        <div>Coding Challenge</div>
+        <ul className="flex-row">
+          <li><Link href='/#'>Receipt</Link></li>
+          <li><Link href='/#solutions'>Solutions</Link></li>
+        </ul>
+      </nav>
       <ReceiptCalculator />
-    </>
+      <Divider my='10' />
+      <ChallengeSolutions />
+      <Divider />
+      <Text fontSize='0.9rem' p='10' align='center'>Made with 🤍 by
+        <a href="http://oscargonzalez.dev" target="_blank" rel="noopener noreferrer"> Oscar Gonzalez</a>
+      </Text>
+    </div>
   )
 }
 
