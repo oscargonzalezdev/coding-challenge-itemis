5a82055667fed1edab0e9ad270c2212a05703824
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-09-30 13:22:35 +0200

    feat(seeds): add seed file containing the products list as an array of objects

---
+++ src/seeds/products.js
@@ -0,0 +1,65 @@
+export const productsArr = [
+    {
+        name: 'book',
+        price: 12.49,
+        category: 'books',
+        isImported: false,
+        imageName: 'book.jpg'
+    },
+    {
+        name: 'music CD',
+        price: 14.99,
+        category: 'general',
+        isImported: false,
+        imageName: 'cd.jpg'
+    },
+    {
+        name: 'chocolate bar',
+        price: 0.85,
+        category: 'food',
+        isImported: false,
+        imageName: 'chocolate-bar.jpg'
+    },
+    {
+        name: 'chocolate box mark 1',
+        price: 10.00,
+        category: 'food',
+        isImported: true,
+        imageName: 'chocolate-box1.jpg'
+    },
+    {
+        name: 'perfume bottle mark 1',
+        price: 47.50,
+        category: 'general',
+        isImported: true,
+        imageName: 'perfume1.jpg'
+    },
+    {
+        name: 'perfume bottle mark 2',
+        price: 27.99,
+        category: 'general',
+        isImported: true,
+        imageName: 'perfume2.jpg'
+    },
+    {
+        name: 'perfume bottle mark 3',
+        price: 18.99,
+        category: 'general',
+        isImported: false,
+        imageName: 'perfume3.jpg'
+    },
+    {
+        name: 'packet of headache pills',
+        price: 9.75,
+        category: 'medical',
+        isImported: false,
+        imageName: 'pills.jpg'
+    },
+    {
+        name: 'chocolate box mark 2',
+        price: 11.25,
+        category: 'food',
+        isImported: true,
+        imageName: 'chocolate-box2.jpg'
+    }
+]
